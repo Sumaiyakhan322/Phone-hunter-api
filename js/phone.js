44df0acb1b84218a8574a7145c778112a02a9d9c
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-28 01:25:06 +0600

    added phone card

--- js/phone.js
+++ js/phone.js
@@ -1,9 +1,12 @@
  function loadData(){
     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     .then(res=>res.json())
-    .then(data=>getdata(data))
+    .then(data=>displayPhones(data.data))
  }
- function getdata(data){
-    console.log(data.data);
+ const displayPhones=(data)=>{
+    // console.log(data.data);
+    data.forEach(phone => {
+     console.log(phone);
+    });
  }
  loadData()
